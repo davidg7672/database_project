f2b841200d7afa446a9ecfed4d51f277fe0f7572
DavidSosa <davidgsosa1@gmail.com>
2025-10-22 16:48:59 -0700

    Configued database

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,22 +1,16 @@
-const { Pool } = require('"pg');
+// connecting the database
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "postgres",
+    database: process.env.DB_NAME || "mydatabase",
+    password: process.env.DB_PASSWORD || "password",
+    port: process.env.DB_PORT || 5432,
     ssl:
         process.env.NODE_ENV === "production"
             ? { rejectUnauthorized: false }
             : false,
 });
 
-// testing connections
-pool.on("connect", () => {
-    console.log("Connected to PostgreSQL");
-});
-
-// error with connection
-pool.on("error", (err) => {
-    console.error("Unexpected error on idle client", err);
-    process.exit(-1);
-});
-
-module.exports = pool;
+export default pool;
