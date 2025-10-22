d86c2e9dc323d3738a184eaeb946727e3155c134
DavidSosa <davidgsosa1@gmail.com>
2025-10-21 18:04:43 -0700

    Added frontend stuff, some boilerplate, and some componenets were made

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import Employees from "./components/Employees/Employees";
+import Jobs from "./components/Jobs/Jobs";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <div className="container row">
+                <Employees />
+                <Jobs />
+            </div>
+        </>
+    );
+}
+
+export default App;
